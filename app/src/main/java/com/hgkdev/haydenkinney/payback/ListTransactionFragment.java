49eda1d447adf3ddff2bf5c2319b06bcc509d1ca
Hayden Kinney <hkinney2011@gmail.com>
2015-03-29 16:40:02 -0500

    Fixed some UI clutter

--- app/src/main/java/com/hgkdev/haydenkinney/payback/ListTransactionFragment.java
+++ app/src/main/java/com/hgkdev/haydenkinney/payback/ListTransactionFragment.java
@@ -14,8 +14,6 @@ import android.widget.AdapterView;
 import android.widget.ListView;
 import android.widget.TextView;
 
-import com.parse.FindCallback;
-import com.parse.ParseException;
 import com.parse.ParseObject;
 import com.parse.ParseQuery;
 import com.parse.ParseUser;
@@ -55,7 +53,6 @@ public class ListTransactionFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_list_transactions, container, false);
-        amountOwed = (TextView) rootView.findViewById(R.id.txtView_AmountOwed);
         transactions = (ListView) rootView.findViewById(R.id.listView_Transactions);
 
 
@@ -75,7 +72,6 @@ public class ListTransactionFragment extends Fragment {
         LoadTransactionsAsync lTA = new LoadTransactionsAsync(getActivity(), transactions, this);
         lTA.execute();
 
-        updateAmountOwed();
         return rootView;
     }
 
@@ -86,28 +82,9 @@ public class ListTransactionFragment extends Fragment {
                 getArguments().getInt(ARG_SECTION_NUMBER));
     }
 
-    private void updateAmountOwed() {
-        RetrieveAndSumAllTransactions();
-    }
 
 
-    public void RetrieveAndSumAllTransactions() {
-        ParseQuery<ParseObject> query = ParseQuery.getQuery("Transaction");
-        query.findInBackground(new FindCallback<ParseObject>() {
-            public void done(List<ParseObject> transactionList, ParseException e) {
-                if ( e == null ) {
-                    sum = 0;
-                    Log.d("transaction", "Retrieved " + transactionList.size() + " transactions");
-                    for( int i = 0; i < transactionList.size(); i++ ) {
-                        sum += transactionList.get( i ).getDouble( "Cost" );
-                    }
-                    amountOwed.setText(String.format( "Amount Owed: $%.2f", sum ) );
-                } else {
-                    Log.d("transaction", "Error: " + e.getMessage());
-                }
-            }
-        });
-    }
+
 
     public void asyncResult(ArrayList<Transaction> trans) {
         transactionsArray = trans;
